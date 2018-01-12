81162a6f120e70adebe2ce6e76b25bc147a1e43a
DoodeItsTom <choe_tom@yahoo.com>
2018-01-12 16:04:31 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("USer flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+  //function
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+}
+
+  // function
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
 }
+
+flipCard(0);
+flipCard(2);
